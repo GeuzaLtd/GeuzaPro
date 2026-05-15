f4a57a9060ad2c288789251eea47f0968d9b81fa
ingabireol <ingabire069@gmail.com>
2026-05-15 13:58:36 +0200

    fix: rename  Team section to Leadership Team

--- components/Team.tsx
+++ components/Team.tsx
@@ -40,7 +40,7 @@ export default function Team({ members }: { members: TeamMember[] }) {
   return (
     <section id="team" className="py-16 md:py-24 bg-gray-50">
       <div className="max-w-7xl mx-auto px-4 md:px-8 lg:px-16">
-        <SectionHeader title="Team" />
+        <SectionHeader title="LEADERSHIP TEAM" />
       </div>
 
       {/* Full-width carousel with edge fade */}
